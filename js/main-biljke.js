100724696e1243659ce03c239f5012510ca5f799
Andjelka90 <46249123+Andjelka90@users.noreply.github.com>
2019-04-22 00:51:57 +0200

    Add files via upload

--- js/main-biljke.js
+++ js/main-biljke.js
@@ -67,6 +67,15 @@ $(".burger").click(function () {
   $("ul").toggle("slow");
 });
 
+$("#back-to-top").hide();
+$(window).scroll(function() {
+    if ($(window).scrollTop() > 100) {
+        $("#back-to-top").fadeIn("slow");
+    }
+    else {
+        $("#back-to-top").fadeOut("fast");
+    }
+});
 
 
 
